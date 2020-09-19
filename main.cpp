de13c8dd16ac7648d921bccdff712437e9691f38
Mikrocotre <71569464+Mikrocotre@users.noreply.github.com>
2020-09-20 02:40:29 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    cout<<"Enter two numbers:\n"<<endl;
+    cin>>a;
+    cin>>b;
+    c=a*b;
+    cout<<"a x b="<<c<<endl;
+    return 0;
+}

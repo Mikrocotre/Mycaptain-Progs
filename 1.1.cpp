40bec4f8a201b1ca2f6979dbfa703546755d26c2
Mikrocotre <71569464+Mikrocotre@users.noreply.github.com>
2020-09-20 02:47:41 +0530

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of int:\t"<<sizeof(int)<<"byte"<<endl;
+    cout<<"Size of float:\t"<<sizeof(float)<<"byte"<<endl;
+    cout<<"Size of double:\t"<<sizeof(double)<<"byte"<<endl;
+    cout<<"Size of char:\t"<< sizeof(char)<<"byte"<<endl;
+
+    return 0;
+}
